ccb454e31a0273c86cf802a17978fdba99a12f17
minasanta <minasanta77@gmail.com>
2022-03-11 00:17:37 +0200

    finsh the project

--- script.js
+++ script.js
@@ -1,42 +1,75 @@
+let a = '';
+let b = '';
+let operation = '';
+const reslut = document.querySelector('.reslut p');
+const btns = Array.from(document.querySelectorAll('button'));
 function add(a,b){
     return a+b;
 }
 function subtract(a,b){
-    return a-b;
+    return b-a;
 }
 function multiply(a,b){
     return a*b;
 }
 function divide(a,b){
-    return a/b;
+    return b/a;
 }
 function operate(op,a,b){
-    a = Number(a);
-    b = Number(b);
+    let num1 = parseFloat(a);
+    let num2 = parseFloat(b);
     switch(op){
-        case '+': add(a,b);
-        case '-': subtract(a,b);
-        case 'x': multiply(a,b);
-        case '/': if(b === 0){return null} else{divide(a,b)};
+        case '+': a = add(num1,num2);break;
+        case '-': a = subtract(num1,num2);break;
+        case '*': a = multiply(num1,num2);break;
+        case '/': if(b === 0){reslut.textContent = 'Error! , click AC and try again'; } else{ a = divide(num1,num2)};break;
         default: return null;
     }
+    a = Math.round(a*1000)/1000;
+    reslut.textContent = a;
+    return a;
+}
+function update(e){
+    reslut.textContent += e.target.textContent.toString();
+}
+function appendNumber(e){
+    let number = e.target.textContent.toString();
+    a+=number;
 }
 
-let a = 0;
-let b = 0;
-let reslut = document.querySelector('.reslut p');
-let btns = Array.from(document.querySelectorAll('button'));
+function appendOperation(e){
+    if(operation!=='')
+        a = operate(operation,a,b);
+    b = a;
+    operation = e.target.textContent.toString();
+    a = '';
+}
+
 btns.forEach(btn => {
-    btn.addEventListener('click',function (){
-        if(btn.id === 'clear')
+    btn.addEventListener('click',function (e) {
+        if(btn.className === 'op')
         {
-            reslut.textContent = " 0 ";
-            a = 0;
-            b = 0;
+            appendOperation(e);
+            update(e);
+        }
+        else if (btn.id === 'clear')
+        {
+            reslut.textContent='';
+            a = '';
+            b = '';
+            operation = '';
+        }
+        else if (btn.id === 'equal')
+        {
+            if(a === ''|| b === ''|| operation==='')
+                reslut.textContent='Error! , click AC & try again';
+            else
+                a = operate(operation,a,b);
         }
         else
         {
-            reslut.textContent = btn.textContent;
+            appendNumber(e);
+            update(e);
         }
     });
 });
